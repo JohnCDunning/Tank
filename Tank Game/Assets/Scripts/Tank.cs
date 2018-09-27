abea226ecbf02d380f92e8cf267253c3dc8def20
John Dunning <John.charles.dunning@gmail.com>
2018-09-27 16:07:20 +1000

    no message

--- Tank Game/Assets/Scripts/Tank.cs
+++ Tank Game/Assets/Scripts/Tank.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Tank : MonoBehaviour {
     public float tankRotateSpeed;
     public float tankSpeed;
-
+    public GameObject Turret;
+    public GameObject marker;
     // Use this for initialization
     void Start() {
 
@@ -13,11 +14,14 @@ public class Tank : MonoBehaviour {
 
     // Update is called once per frame
     void Update()
-    {
+    {        
         MoveTank();
+        TurretRotation();
     }
+    //to move the tank
     void MoveTank()
     {
+
         // instructions for spinning left
         if (Input.GetKey(KeyCode.A))
         {
@@ -40,5 +44,24 @@ public class Tank : MonoBehaviour {
             transform.Translate(Vector3.back * (tankSpeed / 2) * Time.deltaTime);
 
         }
+    }
+    // to rotate turret
+    void TurretRotation()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (Vector3.Distance(transform.position, hit.point) > 1)
+            {
+                Debug.Log(hit.point);
+                marker.transform.position = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+                //rotation
+                Turret.transform.LookAt(marker.transform.position);
+            }
+
+
+        }
+
     }
 }
