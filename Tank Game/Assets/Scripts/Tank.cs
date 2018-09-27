c87992f69b66168bdd0411ebaefc1da4bcb854f5
John Dunning <John.charles.dunning@gmail.com>
2018-09-27 14:22:44 +1000

    tank script prefab

---
+++ Tank Game/Assets/Scripts/Tank.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour {
+    public float tankRotateSpeed;
+    public float tankSpeed;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveTank();
+    }
+    void MoveTank()
+    {
+        // instructions for spinning left
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down * tankRotateSpeed * Time.deltaTime);
+        }
+        //spin right
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * tankRotateSpeed * Time.deltaTime);
+        }
+        //move tank forward
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * (tankSpeed / 2) * Time.deltaTime);
+
+        }
+        //move tank backward
+    }
+}
